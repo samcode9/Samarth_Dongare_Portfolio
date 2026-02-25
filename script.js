60a6737c99d2bf633529e1a585f589e5930368c6
samcode9 <sadongare7@gmail.com>
2026-02-25 13:41:15 +0530

    added new function for the achivement scrolling

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ function toggleTheme() {
 function toggleMenu() {
   document.getElementById("nav-menu").classList.toggle("active");
 }
+const items = document.querySelectorAll(".ach-item");
+const descBox = document.getElementById("ach-desc");
+const descWrapper = document.getElementById("ach-desc-box");
+
+let active = -1;
+
+function updateAchievements() {
+  items.forEach((item, index) => {
+    const rect = item.getBoundingClientRect();
+    const trigger = window.innerHeight * 0.55; // better visual midpoint
+
+    if (rect.top < trigger && rect.bottom > trigger) {
+
+      if (active !== index) {
+        active = index;
+
+        const newText = item.getAttribute("data-desc");
+
+        descWrapper.classList.add("fade-out");
+
+        setTimeout(() => {
+          descBox.textContent = newText;
+          descWrapper.classList.remove("fade-out");
+          descWrapper.classList.add("fade-in");
+        }, 150);
+      }
+    }
+  });
+}
+
+window.addEventListener("scroll", updateAchievements);
